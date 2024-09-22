2f576a0fbaf255601eacab90eba3d8f41a2e773e
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2024-09-22 17:54:18 +0600

    added history btn

--- js/index.js
+++ js/index.js
@@ -51,3 +51,33 @@ document
     setValueToInner("remaining-balance", remaining);
   });
 // toggle btn
+const historyTab = document.getElementById("history-tab");
+const assistantTab = document.getElementById("assistant-tab");
+historyTab.addEventListener("click", function () {
+  historyTab.classList.add(
+    "text-white",
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600"
+  );
+  assistantTab.classList.remove(
+    "text-white",
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600"
+  );
+});
+assistantTab.addEventListener("click", function () {
+  assistantTab.classList.add(
+    "text-white",
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600"
+  );
+  historyTab.classList.remove(
+    "text-white",
+    "bg-gradient-to-r",
+    "from-blue-500",
+    "to-purple-600"
+  );
+});
