fddb78ee4795a0d52c8264acc1f4a3485b1cb1f0
Rahul Khan Suvo <rahul.khan.suvo@gmail.com>
2024-09-22 17:53:31 +0600

    added function for calculate saving

--- js/index.js
+++ js/index.js
@@ -23,3 +23,31 @@ inputValidation("software", "software-error");
 inputValidation("courses", "courses-error");
 inputValidation("internet", "internet-error");
 // calculate saving
+document
+  .getElementById("calculate-savings")
+  .addEventListener("click", function () {
+    const income = getInputValue("income");
+    const software = getInputValue("software");
+    const courses = getInputValue("courses");
+    const internet = getInputValue("internet");
+    const totalCost = software + courses + internet;
+    const balance = income - totalCost;
+    const savingPercentage = getInputValue("savings") || 0;
+    if (isNaN(savingPercentage)) {
+      removeHidden("logic-error");
+      return;
+    } else {
+      setHidden("logic-error");
+    }
+    const saving = (balance * savingPercentage) / 100;
+    const remaining = balance - saving;
+    if (isNaN(balance) || income < totalCost) {
+      removeHidden("logic-error");
+      return;
+    } else {
+      setHidden("logic-error");
+    }
+    setValueToInner("savings-amount", saving);
+    setValueToInner("remaining-balance", remaining);
+  });
+// toggle btn
